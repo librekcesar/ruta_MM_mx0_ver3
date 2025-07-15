b115e00c84c42b01bc4adae6cc55286985044c9b
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 17:09:31 -0600

    Add files via upload

---
+++ estaciones_mx0_ver3_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_ver3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Art 127 Constitucional 3935, Frutos de la Revolucion, 96470 Coatzacoalcos, Ver., México", "description": "VER3", "t_estacion": "Destino" }, "geometry": { "type": "Point", "coordinates": [ -94.425503, 18.1303959, 0.0 ] } }
+]
+}
---
+++ paradas_mx0_ver3_copia.js
@@ -0,0 +1,11 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_mx0_ver3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "km 118+560, La Tinaja - Acayucan, 95658 Isla, Ver.", "layer": "PEMEX_MX0_VER3", "direccion": "km 118+560, La Tinaja - Acayucan, 95658 Isla, Ver." }, "geometry": { "type": "Point", "coordinates": [ -95.536127, 18.1556137, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Estación de Servicio Pemex", "description": "Aut. Orizaba-Cordoba Km 273+620 Ex Hacienda San Antonio, 94390 Orizaba, Ver.", "layer": "PEMEX_MX0_VER3", "direccion": "Aut. Orizaba-Cordoba Km 273+620 Ex Hacienda San Antonio, 94390 Orizaba, Ver." }, "geometry": { "type": "Point", "coordinates": [ -97.0795129, 18.8384922, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Gasolinera Pemex SG Gasolineros", "description": "México 150D, 72680 San Francisco Ocotlán, Pue.", "layer": "PEMEX_MX0_VER3", "direccion": "México 150D, 72680 San Francisco Ocotlán, Pue." }, "geometry": { "type": "Point", "coordinates": [ -98.2776772, 19.1405144, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Pemex", "description": "Minatitlán - Cordoba, Tierra Adentro, 95200 Ver.", "layer": "PEMEX_MX0_VER3", "direccion": "Minatitlán - Cordoba, Tierra Adentro, 95200 Ver." }, "geometry": { "type": "Point", "coordinates": [ -96.2809347, 18.6514261, 0.0 ] } }
+]
+}
